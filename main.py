ba12814515035e8e2c32e64dba2ed9dbd80a3b56
Vasiliy <w_av@mail.ru>
2024-01-11 14:54:42 +0300

    add arguments run app

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import argparse
+
 from aiohttp import web
 
 from routes import setup_routes
 
-app = web.Application()
-setup_routes(app)
-web.run_app(app)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="aiohttp server example")
+    parser.add_argument('--path', default="")
+    parser.add_argument('--port', default=8082)
+    args = parser.parse_args()
+
+    app = web.Application()
+    setup_routes(app)
+
+    kwargs = {}
+    if args.port:
+        kwargs.update({'port': args.port})
+
+    if args.path:
+        kwargs.update({'path': args.path})
+
+    web.run_app(app, **kwargs)
